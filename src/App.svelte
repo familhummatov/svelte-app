368683746afafb11c8c32c5a32179b522d8d1c97
Famil Hümmətov <famil.hummetov@cybernet.az>
2021-04-30 23:31:14 +0400

    new

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+  import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
+
+  let players = [
+    { name: "Darth Maul", points: 89 },
+    { name: "Luke Skywalker", points: 99 },
+    { name: "Darth Vader", points: 96 },
+    { name: "Obi Van Kenobi", points: 91 },
+  ];
+
+  const addPlayer = (e) => {
+    players = [...players, e.detail];
+  };
+
+  const deletePlayer = (e) => {
+    players.splice(e.detail, 1);
+    players = [...players];
+  };
+</script>
+
+<main class="container">
+  <Navbar />
+  <AddPlayer on:addplayer={addPlayer} />
+
+  {#each players as player, i}
+    <Player
+      on:delete={deletePlayer}
+      index={i}
+      name={player.name}
+      points={player.points}
+    />
+  {:else}
+    <h3>No players</h3>
+  {/each}
+</main>
---
+++ src/Player.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let name;
+  export let points;
+  export let index;
+  let showControls = false;
+
+  const increase = () => (points += 1);
+  const decrease = () => (points -= 1);
+  const toggle = () => (showControls = !showControls);
+</script>
+
+<div class="card">
+  <h1 class="">
+    {name}
+    <button class="btn btn-sm" on:click={toggle}
+      >{#if showControls}-{:else}+{/if}</button
+    >
+    <button
+      class="btn btn-danger btn-sm"
+      on:click={() => dispatch("delete", index)}>x</button
+    >
+  </h1>
+
+  <h3>Points: {points}</h3>
+  {#if showControls}
+    <button class="btn" on:click={increase}>+1</button>
+    <button class="btn btn-dark" on:click={decrease}>-1</button>
+    <input type="number" bind:value={points} />
+  {/if}
+</div>
+
+<style>
+  h1 {
+    color: #204f6e;
+  }
+  h3 {
+    margin-bottom: 10px;
+  }
+</style>
